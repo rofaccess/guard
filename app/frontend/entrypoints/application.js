8494d049fdc908beca813eda8cca2089a98a6a81
Rodrigo Fernandez <rofaccess@gmail.com>
2024-08-15 11:27:00 -0400

    Update home view adding Vue component

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,28 +1,17 @@
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
+import { createApp } from 'vue/dist/vue.esm-bundler';
+import ButtonCounter from '../components/ButtonCounter.vue'
 
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
+const app = createApp({
+    data() {
+        return {
+            message: 'Vite, Vue and Rails'
+        }
+    }
+})
 
+app.component('ButtonCounter', ButtonCounter)
+
+app.mount('#app');
+
+console.log('Vite, Vue and Rails')
 console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
-
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
